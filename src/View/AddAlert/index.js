ea6b6131e1fe63765b3f19f6fc9ebc1d0b2856a7
avoca <tran.chaugiangk39@gmail.com>
2020-11-17 14:34:42 +0700

    v1

---
+++ src/View/AddAlert/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../../assets/image/logo.svg';
+
+import './index.css';
+import '../../App.css';
+class AnimationCircle extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        this is React^^
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default AnimationCircle;
