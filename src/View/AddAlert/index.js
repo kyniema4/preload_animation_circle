19557a1acd144de4aab9561d42707ff2b27b97ad
NguyenDinhTien <tomtc95@gmail.com>
2020-11-18 18:58:39 +0700

    commit

--- src/View/AddAlert/index.js
+++ src/View/AddAlert/index.js
@@ -7,96 +7,80 @@ import videoBg from "../../assets/image/video.mp4";
 import { LoadingOutlined } from "@ant-design/icons";
 
 class AnimationCircle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible:false,
-            visibleFirst: true,
-            visibleSecond: false,
-            visibleThird: false,
-            listItem:[],
-            currentIndex:0,
-            arrayTypeData:[
-                {
-                    name:"Type_Message",
-                    value:1
-                },
-                {
-                    name:"Type_Choice",
-                    value:2
-                },
-                {
-                    name:"Type_Input",
-                    value:3
-                }
-            ],
-        };
-    }
-    componentDidMount() {
-        this.getListData()
-        // setTimeout(this.showFirst, 10000);
-    }
-
-    getListData() {
-        fetch('http://localhost:3000/data',{
-            method: 'GET',
-        }).then(res=>{
-            return res.json()
-        }).then(res=>{
-
-            if(res.items.length)
-            {
-                this.setState({listItem: res.items})
-            }
-            else
-            {
-                message.error('Error from server')
-            }
-           
-        }).catch(err=>{
-            console.log(err)
-            message.error(err.message || 'Error from server')
-        })
-    }
-
-    clickStart(){
-        this.setState({visibleFirst:false})
-        this.setData(this.state.listItem, 0)
-    }
-
-    setData(array, index){
-
-        for(let i=index; i < array.length; i++)
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      visibleFirst: true,
+      visibleSecond: false,
+      visibleThird: false,
+      listItem: [],
+      currentIndex: 0,
+      arrayTypeData: [
         {
-            if(array[i].data.length)
-            {
-                let dataValue = array[i].data.find(item => item.dataKey === "Data")
-                let voiceValue = array[i].data.find(item => item.dataKey === "Voice")
-                let orderValue = array[i].data.find(item => item.dataKey === "Order")
+          name: "Type_Message",
+          value: 1,
+        },
+        {
+          name: "Type_Choice",
+          value: 2,
+        },
+        {
+          name: "Type_Input",
+          value: 3,
+        },
+      ],
+    };
+  }
+  componentDidMount() {
+    this.getListData();
+    // setTimeout(this.showFirst, 10000);
+  }
 
-                if(dataValue || voiceValue || orderValue)
-                {
-                    let typeData
-                    this.state.arrayTypeData.forEach(type =>{
-                        if(dataValue.dataVal[type.name])
-                        {
-                            typeData = type
-                        }
-                    })
+  getListData() {
+    fetch("http://localhost:3000/data", {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.items.length) {
+          this.setState({ listItem: res.items });
+        } else {
+          message.error("Error from server");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        message.error(err.message || "Error from server");
+      });
+  }
 
-                    console.log(dataValue)
-                    console.log(voiceValue)
-                    console.log(orderValue)
+  clickStart() {
+    this.setState({ visibleFirst: false });
+    this.setData(this.state.listItem, 0);
+  }
 
-                    this.setState({currentItem: array[i], currentIndex:index, 
-                        dataValue : dataValue.dataVal[typeData.name], voiceValue : voiceValue.dataVal
-                        ,orderValue: orderValue.dataVal, typeData, visible:true})
-                    break;
-                }
+  setData(array, index) {
+    for (let i = index; i < array.length; i++) {
+      if (array[i].data.length) {
+        let dataValue = array[i].data.find((item) => item.dataKey === "Data");
+        let voiceValue = array[i].data.find((item) => item.dataKey === "Voice");
+        let orderValue = array[i].data.find((item) => item.dataKey === "Order");
 
-              
+        if (dataValue || voiceValue || orderValue) {
+          let typeData;
+          this.state.arrayTypeData.forEach((type) => {
+            if (dataValue.dataVal[type.name]) {
+              typeData = type;
             }
-        };
+          });
+
+          console.log(dataValue);
+          console.log(voiceValue);
+          console.log(orderValue);
+
           this.setState({
             currentItem: array[i],
             currentIndex: index,
@@ -108,82 +92,126 @@ class AnimationCircle extends React.Component {
           });
           break;
         }
-      
-    
-
-    nextModal(){
-        this.setState({visible:false})
-        setTimeout(() => {
-            this.setData(this.state.listItem, this.state.currentIndex + 1)
-        }, 500);
+      }
     }
+  }
 
-    handleEnded()
-    {
-        if(this.state.dataValue.action_type === 'automatic')
-        {
-            this.nextModal()
-        }
+  nextModal() {
+    this.setState({ visible: false });
+    setTimeout(() => {
+      this.setData(this.state.listItem, this.state.currentIndex + 1);
+    }, 500);
+  }
+
+  handleEnded() {
+    if (this.state.dataValue.action_type === "automatic") {
+      this.nextModal();
     }
+  }
 
-    render() {
+  render() {
+    const {
+      visible,
+      visibleFirst,
+      currentItem,
+      dataValue,
+      voiceValue,
+      typeData
+    } = this.state;
+    return (
+      <div className="container">
+        <video autoPlay="autoplay" loop="loop" muted className="video">
+          <source src={videoBg} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="div-loading">
+          <LoadingOutlined className="loading-icon" />
+        </div>
 
-        const { visible, visibleFirst, currentItem, dataValue, voiceValue } = this.state
-        return (
-            <div className='container'>
-                <video autoPlay="autoplay"  loop="loop" muted className='video' >
-                    <source src={videoBg} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <div className="div-loading">
-                    <LoadingOutlined className="loading-icon" />
-                </div>
+        <Rodal
+          height={200}
+          width={400}
+          visible={visibleFirst}
+          showCloseButton={false}
+          animation="slideUp"
+          duration="1500"
+        >
+          <div className="div-button">
+            <Button
+              className="mr15 right-btn"
+              onClick={() => this.clickStart()}
+            >
+              Click to start
+            </Button>
+          </div>
+        </Rodal>
 
-                <Rodal
-                    height={200}
-                    width={400}
-                    visible={visibleFirst}
-                    showCloseButton={false}
-                    animation='slideUp'
-                    duration ='1500'
+        {currentItem ? (
+          <Rodal
+            height={200}
+            width={400}
+            visible={visible}
+            showCloseButton={false}
+            animation="slideUp"
+            duration="1500"
+          >
+            <audio
+              autoPlay="autoplay"
+              onEnded={() => this.handleEnded()}
+              src={voiceValue}
+            ></audio>
+            {/* <iframe title='iframe' allow="autoplay" src={voiceValue} style={{display: 'none'}} ></iframe> */}
+            {Array.isArray(dataValue.title) ? (
+              <p className="content-modal">{dataValue.title[0]}</p>
+            ) : (
+              <p className="content-modal">{dataValue.title}</p>
+            )}
+
+             
+
+
+            <div className="div-button">
+              {dataValue.action ? (
+                <Button
+                  className="mr15 left-btn"
+                  onClick={() => this.nextModal()}
+                >
+                  {dataValue.action}
+                </Button>
+              ) : (
+                ""
+              )}
+
+              {dataValue.action_type === "manual" &&
+                typeData.value === 2 &&
+                dataValue.answer &&
+                dataValue.answer.map((item, index) =>
+                  index % 2 == 0 ? (
+                    <Button
+                      key={index}
+                      className="mr15 left-btn"
+                      onClick={() => this.nextModal()}
                     >
-
-                    <div className="div-button">
-                        <Button className="mr15 right-btn" onClick={() => this.clickStart()}>Click to start</Button>
-                    </div>
-                    </Rodal>
-                
-                {currentItem ? (
-                    <Rodal
-                    height={200}
-                    width={400}
-                    visible={visible}
-                    showCloseButton={false}
-                    animation='slideUp'
-                    duration ='1500'
+                      {item.field}
+                    </Button>
+                  ) : (
+                    <Button
+                      key={index}
+                      className="mr15 right-btn"
+                      onClick={() => this.nextModal()}
                     >
-
-                    <audio autoPlay="autoplay" onEnded={() => this.handleEnded()} src={voiceValue}></audio>
-                    {/* <iframe title='iframe' allow="autoplay" src={voiceValue} style={{display: 'none'}} ></iframe> */}
-                    {Array.isArray(dataValue.title) ? (
-                        <p className="content-modal">{dataValue.title[0]}</p>
-                    ) :(
-                        <p className="content-modal">{dataValue.title}</p>
-                    )}
-
-                    <div className="div-button">
-                        {dataValue.action ? (
-                            <Button className="mr15 left-btn" onClick={() => this.nextModal()}>{dataValue.action}</Button>
-                        ):''}
-
-                        {/* <Button className="mr15 left-btn" onClick={() => this.nextModal()}>English</Button>
-                        <Button className="right-btn" onClick={() => this.nextModal()}>Francais</Button> */}
-                    </div>
-                    </Rodal>
-                ): ''}
+                      {item.field}
+                    </Button>
+                  )
+                )}
             </div>
-        );
-    }
+          </Rodal>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
 export default AnimationCircle;
