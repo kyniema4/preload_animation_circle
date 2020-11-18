17a856d06a72aafbc433ad6083cea77784a318b1
NguyenDinhTien <tomtc95@gmail.com>
2020-11-18 19:00:34 +0700

    Revert "Merge branch 'master' of https://github.com/kyniema4/preload_animation_circle"
    This reverts commit 627db743575e2918bfbbda0489f718b9d6585dd5, reversing
    changes made to 19557a1acd144de4aab9561d42707ff2b27b97ad.

--- src/View/AddAlert/index.js
+++ src/View/AddAlert/index.js
@@ -1,105 +1,86 @@
-import React from 'react';
-import { Button, message } from 'antd';
-import './index.css';
-import 'rodal/lib/rodal.css';
-import Rodal from 'rodal';
-import videoBg from '../../assets/image/video.mp4';
-import { LoadingOutlined } from '@ant-design/icons';
-import doc from '../../doc.js'
+import React from "react";
+import { Form, Input, Button, message } from "antd";
+import "./index.css";
+import "rodal/lib/rodal.css";
+import Rodal from "rodal";
+import videoBg from "../../assets/image/video.mp4";
+import { LoadingOutlined } from "@ant-design/icons";
 
 class AnimationCircle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visibleLoading:false,
-            visible:false,
-            visibleFirst: true,
-            visibleSecond: false,
-            visibleThird: false,
-            listItem:[],
-            currentIndex:0,
-            arrayTypeData:[
-                {
-                    name:"Type_Message",
-                    value:1
-                },
-                {
-                    name:"Type_Choice",
-                    value:2
-                },
-                {
-                    name:"Type_Input",
-                    value:3
-                }
-            ],
-        };
-    }
-    componentDidMount() {
-        this.getListData()
-        // setTimeout(this.showFirst, 10000);
-    }
-
-    getListData() {
-        this.setState({listItem:doc.data.items})
-        // fetch('http://localhost:3000/data',{
-        //     method: 'GET',
-        // }).then(res=>{
-        //     return res.json()
-        // }).then(res=>{
-
-        //     if(res.items.length)
-        //     {
-        //         this.setState({listItem: res.items})
-        //     }
-        //     else
-        //     {
-        //         message.error('Error from server')
-        //     }
-           
-        // }).catch(err=>{
-        //     console.log(err)
-        //     message.error(err.message || 'Error from server')
-        // })
-    }
-
-    clickStart(){
-        this.setState({visibleFirst:false})
-        this.setData(this.state.listItem, 0)
-    }
-
-    setData(array, index){
-        console.log(array)
-        for(let i=index; i < array.length; i++)
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      visibleFirst: true,
+      visibleSecond: false,
+      visibleThird: false,
+      listItem: [],
+      currentIndex: 0,
+      arrayTypeData: [
         {
-            if(array[i].data.length)
-            {
-                let dataValue = array[i].data.find(item => item.dataKey === "Data")
-                let voiceValue = array[i].data.find(item => item.dataKey === "Voice")
-                let orderValue = array[i].data.find(item => item.dataKey === "Order")
+          name: "Type_Message",
+          value: 1,
+        },
+        {
+          name: "Type_Choice",
+          value: 2,
+        },
+        {
+          name: "Type_Input",
+          value: 3,
+        },
+      ],
+    };
+  }
+  componentDidMount() {
+    this.getListData();
+    // setTimeout(this.showFirst, 10000);
+  }
 
-                if(dataValue || voiceValue || orderValue)
-                {
-                    let typeData
-                    this.state.arrayTypeData.forEach(type =>{
-                        if(dataValue.dataVal[type.name])
-                        {
-                            typeData = type
-                        }
-                    })
+  getListData() {
+    fetch("http://localhost:3000/data", {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.items.length) {
+          this.setState({ listItem: res.items });
+        } else {
+          message.error("Error from server");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        message.error(err.message || "Error from server");
+      });
+  }
 
-                    console.log(dataValue)
-                    console.log(voiceValue)
-                    console.log(orderValue)
+  clickStart() {
+    this.setState({ visibleFirst: false });
+    this.setData(this.state.listItem, 0);
+  }
 
-                    this.setState({currentItem: array[i], currentIndex:index, 
-                        dataValue : dataValue.dataVal[typeData.name], voiceValue : voiceValue.dataVal
-                        ,orderValue: orderValue.dataVal, typeData, visible:true, visibleLoading:false})
-                    break;
-                }
+  setData(array, index) {
+    for (let i = index; i < array.length; i++) {
+      if (array[i].data.length) {
+        let dataValue = array[i].data.find((item) => item.dataKey === "Data");
+        let voiceValue = array[i].data.find((item) => item.dataKey === "Voice");
+        let orderValue = array[i].data.find((item) => item.dataKey === "Order");
 
-              
+        if (dataValue || voiceValue || orderValue) {
+          let typeData;
+          this.state.arrayTypeData.forEach((type) => {
+            if (dataValue.dataVal[type.name]) {
+              typeData = type;
             }
-        };
+          });
+
+          console.log(dataValue);
+          console.log(voiceValue);
+          console.log(orderValue);
+
           this.setState({
             currentItem: array[i],
             currentIndex: index,
@@ -111,20 +92,7 @@ class AnimationCircle extends React.Component {
           });
           break;
         }
-      
-    
-
-    nextModal(){
-
-        if(document.getElementById('audio'))
-        {
-            document.getElementById('audio').pause()
-        }
-
-        this.setState({visible:false, visibleLoading:true})
-        setTimeout(() => {
-            this.setData(this.state.listItem, this.state.currentIndex + 1)
-        }, 500);
+      }
     }
   }
 
@@ -139,30 +107,90 @@ class AnimationCircle extends React.Component {
     if (this.state.dataValue.action_type === "automatic") {
       this.nextModal();
     }
+  }
 
-    render() {
+  render() {
+    const {
+      visible,
+      visibleFirst,
+      currentItem,
+      dataValue,
+      voiceValue,
+      typeData
+    } = this.state;
+    return (
+      <div className="container">
+        <video autoPlay="autoplay" loop="loop" muted className="video">
+          <source src={videoBg} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="div-loading">
+          <LoadingOutlined className="loading-icon" />
+        </div>
 
-        const { visible, visibleFirst, currentItem, dataValue, voiceValue, typeData, visibleLoading } = this.state
-        return (
-            <div className='container'>
-                <video autoPlay="autoplay"  loop="loop" muted className='video' >
-                    <source src={videoBg} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+        <Rodal
+          height={200}
+          width={400}
+          visible={visibleFirst}
+          showCloseButton={false}
+          animation="slideUp"
+          duration="1500"
+        >
+          <div className="div-button">
+            <Button
+              className="mr15 right-btn"
+              onClick={() => this.clickStart()}
+            >
+              Click to start
+            </Button>
+          </div>
+        </Rodal>
 
-                {visibleLoading ? (
-                    <div className="div-loading">
-                        <LoadingOutlined className="loading-icon" />
-                    </div>
-                ):''}
+        {currentItem ? (
+          <Rodal
+            height={200}
+            width={400}
+            visible={visible}
+            showCloseButton={false}
+            animation="slideUp"
+            duration="1500"
+          >
+            <audio
+              autoPlay="autoplay"
+              onEnded={() => this.handleEnded()}
+              src={voiceValue}
+            ></audio>
+            {/* <iframe title='iframe' allow="autoplay" src={voiceValue} style={{display: 'none'}} ></iframe> */}
+            {Array.isArray(dataValue.title) ? (
+              <p className="content-modal">{dataValue.title[0]}</p>
+            ) : (
+              <p className="content-modal">{dataValue.title}</p>
+            )}
 
-                <Rodal
-                    height={200}
-                    width={400}
-                    visible={visibleFirst}
-                    showCloseButton={false}
-                    animation='slideUp'
-                    duration ='1500'
+             
+
+
+            <div className="div-button">
+              {dataValue.action ? (
+                <Button
+                  className="mr15 left-btn"
+                  onClick={() => this.nextModal()}
+                >
+                  {dataValue.action}
+                </Button>
+              ) : (
+                ""
+              )}
+
+              {dataValue.action_type === "manual" &&
+                typeData.value === 2 &&
+                dataValue.answer &&
+                dataValue.answer.map((item, index) =>
+                  index % 2 == 0 ? (
+                    <Button
+                      key={index}
+                      className="mr15 left-btn"
+                      onClick={() => this.nextModal()}
                     >
                       {item.field}
                     </Button>
